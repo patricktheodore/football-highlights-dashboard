a6c2e333e96873c122438e12e54175fc6714f019
patricktheodore <patricktheodoresara@gmail.com>
2021-10-04 16:04:12 +0800

    embedded videos and team icons loaded

--- assets/js/main-display.js
+++ assets/js/main-display.js
@@ -4,7 +4,7 @@ const fixturesContainerDiv = $('.fixtures-table');
 const leagueTableContainer = $('.league-table');
 const dateToday = moment().format('YYYY-MM-DD');
 
-let teamID = '';
+
 
 let userTeamName = localStorage.getItem('team'); //need to edit team name by dropping FC off the end. 
 let teamName = userTeamName.replace(" FC", "");
@@ -15,6 +15,14 @@ teamNameEl.text(teamName + "'s Recent Games.");
 //get team name from local storage
 //display it as the title of the page
 
+function getHighlightVideo(videos) {
+    return videos.filter(function (video) {
+        return video.title.toLowerCase() === 'highlights';
+    })
+}
+
+
+
 function getHighlights() {
     fetch("https://www.scorebat.com/video-api/v3/")
         .then(function (response) {
@@ -24,7 +32,8 @@ function getHighlights() {
             return response.json();
         })
         .then(function (data) {
-            
+            console.log(data);
+
             //search data for teamName
             for (let i = 0; i < data.response.length; i++) {
                 if (data.response[i].title.includes(teamName)) {
@@ -38,25 +47,32 @@ function getHighlights() {
                     //create a li
                     const gameTitleItem = $('<li>');
                     //create a button with text(highlights) and href matchviewURL
-                    const gameTitleItemBtn = $('<a>').text("highlights").attr('href', data.response[i].matchviewUrl);
 
                     gameContainerDiv.append(
                         gameDiv.append(
                             gameTitle.append(
                                 gameTitleList.append(
-                                    gameTitleItem,
-                                        gameTitleItemBtn)
+                                    gameTitleItem)
                             )
                         )
                     );
-                }                 
-            }
 
+                    const highlightVideos = getHighlightVideo(data.response[i].videos);
+
+
+                    if (highlightVideos.length === 1) {
+                        const videoContainer = document.createElement('div');
+                        videoContainer.setAttribute('style', 'width: 400px');
+                        videoContainer.innerHTML = highlightVideos[0].embed;
+                        gameTitleList.append(videoContainer);
+                    }
+                }
             }
+        }
         );
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     getHighlights();
     getStandings();
 });
@@ -82,8 +98,8 @@ function getStandings() {
 
                 //set teamID to teams id in array. 
                 if (data.standings[0].table[i].team.name === userTeamName) {
-                    teamID = data.standings[0].table[i].team.id;
-                    getFixtures(teamID); 
+                    const teamID = data.standings[0].table[i].team.id;
+                    getFixtures(teamID);
                 }
 
                 //create a table row
@@ -93,42 +109,62 @@ function getStandings() {
                 //td for team crest image
                 const teamCrestContainer = $('<td>');
                 //img with src of crest url
-                const teamCrestImg = $('<img>').attr({src:data.standings[0].table[i].team.crestUrl, class:"team-crest-img"});
+                const teamCrestImg = $('<img>').attr({ src: data.standings[0].table[i].team.crestUrl, class: "team-crest-img" });
                 //td with team name
-                const teamName = $('<td>').text(data.standings[0].table[i].team.name).attr({class:"team-name"});
+                const teamName = $('<td>').text(data.standings[0].table[i].team.name).attr({ class: "team-name" });
                 //span with team form
-                const teamForm = $('<span>').attr({class:"teamForm"});
+                const teamForm = $('<span>').attr({ class: "teamForm" });
                 //p with games won
-                const teamWins = $('<span>').text(data.standings[0].table[i].won).attr({class:"wins"});
+                const teamWins = $('<span>').text(data.standings[0].table[i].won).attr({ class: "wins" });
                 //td with games drawn
-                const teamDraws = $('<span>').text(data.standings[0].table[i].draw).attr({class:"draws"});
+                const teamDraws = $('<span>').text(data.standings[0].table[i].draw).attr({ class: "draws" });
                 //td with games lost
-                const teamLosses = $('<span>').text(data.standings[0].table[i].lost).attr({class:"losses"});
+                const teamLosses = $('<span>').text(data.standings[0].table[i].lost).attr({ class: "losses" });
                 //td with games played
                 const teamPlayedGames = $('<td>').text(data.standings[0].table[i].playedGames);
                 //td goals diff
                 const teamGoalsDiff = $('<td>').text(data.standings[0].table[i].goalDifference);
                 //td team points
-                const teamPoints = $('<td>').text(data.standings[0].table[i].points).attr({class:"points"});
+                const teamPoints = $('<td>').text(data.standings[0].table[i].points).attr({ class: "points" });
 
                 leagueTableContainer.append(
-                        tableRow.append(
-                            teamPos, 
-                            teamCrestContainer.append(teamCrestImg),
-                            teamName.append(teamForm.append(
+                    tableRow.append(
+                        teamPos,
+                        teamCrestContainer.append(teamCrestImg),
+                        teamName.append(teamForm.append(
                             teamWins,
                             teamDraws,
                             teamLosses)),
-                            teamPlayedGames,
-                            teamGoalsDiff,
-                            teamPoints
+                        teamPlayedGames,
+                        teamGoalsDiff,
+                        teamPoints
                     )
                 );
             }
         });
 }
 
-function getFixtures() {
+function getTeamLogo(teamID){
+    return fetch(`http://api.football-data.org/v2/teams/${teamID}`, {
+        headers: {
+            'X-Auth-Token': "d9a5e68af1764fc0acc74a34bc2ebb48"
+        },
+    })
+    .then((response) => response.json())
+    .then(function (result){
+        return result.crestUrl
+    })
+}
+
+function createTeamName(name, icon){
+    const iconImg = $("<img>")
+    iconImg.attr({src: icon, class: "team-crest-img"})
+    const team = $('<td>').text(name);
+    team.append(iconImg)
+    return team
+}
+
+function getFixtures(teamID) {
     fetch(`http://api.football-data.org/v2/teams/${teamID}/matches?status=SCHEDULED&dateFROM=${dateToday}`, {
         headers: {
             'X-Auth-Token': "d9a5e68af1764fc0acc74a34bc2ebb48"
@@ -140,14 +176,19 @@ function getFixtures() {
             }
             return response.json();
         })
-        .then(function (data) {
+        .then(async function (data) {
             console.log(data);
             for (let i = 0; i < 3; i++) {
+                const match = data.matches[i]
                 const tableRow = $('<tr>');
-                const competition = $('<td>').text(data.matches[i].competition.name);
-                const homeTeam = $('<td>').text(data.matches[i].homeTeam.name);
+                const competition = $('<td>').text(match.competition.name);
+                const homeTeamIcon = await getTeamLogo(match.homeTeam.id);
+                const awayTeamIcon = await getTeamLogo(match.awayTeam.id);
+
+                const homeTeam = createTeamName(match.homeTeam.name, homeTeamIcon);
+                const awayTeam = createTeamName(match.awayTeam.name, awayTeamIcon);
+
                 const vsEl = $('<td>').text(' vs ');
-                const awayTeam = $('<td>').text(data.matches[i].awayTeam.name)
 
                 fixturesContainerDiv.append(
                     tableRow.append(
@@ -156,7 +197,7 @@ function getFixtures() {
                         vsEl,
                         awayTeam
                     )
-                );               
-            }          
+                );
+            }
         });
 };
